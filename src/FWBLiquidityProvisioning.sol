1f6aa1e0d269dd7fc8c07256e4cdaa243d4f8d00
Rajath Alex <rajathalex@gmail.com>
2022-05-02 17:25:02 -0400

    Renaming contracts

--- src/FWBLiquidityProvisioning.sol
+++ src/FWBLiquidityProvisioning.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity ^0.8.10;
+pragma solidity ^0.8.12;
 
-contract Contract {
+contract FWBLiquidityProvisioning {
     uint256 public num;
 
     constructor(uint256 _num) {
--- src/test/FWBLiquidityProvisioning.t.sol
+++ src/test/FWBLiquidityProvisioning.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity ^0.8.10;
+pragma solidity ^0.8.12;
 
 // testing libraries
 import "@ds/test.sol";
@@ -8,15 +8,15 @@ import {stdCheats} from "@std/stdlib.sol";
 import {Vm} from "@std/Vm.sol";
 import {DSTestPlus} from "@solmate/test/utils/DSTestPlus.sol";
 
-import {Contract} from "../Contract.sol";
+import {FWBLiquidityProvisioning} from "../FWBLiquidityProvisioning.sol";
 
-contract RouterTest is DSTestPlus, stdCheats {
+contract FWBLiquidityProvisioningTest is DSTestPlus, stdCheats {
     Vm private vm = Vm(HEVM_ADDRESS);
-    Contract public numContract;
+    FWBLiquidityProvisioning public numContract;
     uint256 public constant originalNumber = 7;
 
     function setUp() public {
-        numContract = new Contract(originalNumber);
+        numContract = new FWBLiquidityProvisioning(originalNumber);
         vm.label(address(numContract), "Contract");
     }
 
