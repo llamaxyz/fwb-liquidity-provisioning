34ed305819b793a6d5e6d5ee1bb446f1cd308257
Rajath Alex <rajathalex@gmail.com>
2022-05-04 16:53:31 -0400

    Adding WETH

--- src/FWBLiquidityProvisioningEscrow.sol
+++ src/FWBLiquidityProvisioningEscrow.sol
@@ -8,6 +8,7 @@ contract FWBLiquidityProvisioningEscrow {
     // Temporarily setting WBTC-ETH Gamma Vault as placeholder -> Set later as FWB-ETH Gamma Vault
     IHypervisor private constant GAMMA_FWB_VAULT = IHypervisor(0x35aBccd8e577607275647edAb08C537fa32CC65E);
     IERC20 private constant FWB = IERC20(0x35bD01FC9d6D5D81CA9E055Db88Dc49aa2c699A8);
+    IERC20 private constant WETH = IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
 
     address private constant LLAMA_MULTISIG = 0xA519a7cE7B24333055781133B13532AEabfAC81b;
     address private constant FWB_MULTISIG = 0x660F6D6c9BCD08b86B50e8e53B537F2B40f243Bd;
